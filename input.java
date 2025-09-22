dda15b92418b94c2211b56b5bf108c2c520a9739
manerutuja22 <manerutuja719@gmail.com>
2025-09-22 18:14:46 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class input{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        int num = sc.nextInt();
+        
+        for(int num1=0;num1>=4;num1++){
+            System.out.println(num1);
+        }
+        System.out.println(num);
+    }
+}
